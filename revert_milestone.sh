34101041c6d75e9d629d9a21d92668f005f005db
Ido Fishler <idofishler@gmail.com>
2013-08-22 12:47:39 +0300

    Added support for revert to milestone
    Also possible to view specific mile stone is added to view_milestones.sh but currently not is use

---
+++ revert_milestone.sh
@@ -0,0 +1,39 @@
+#! /bin/tcsh
+
+# get script running dir
+set cg_folder = ~/.cg
+set meta_file_name = ".cg"
+
+# usage
+if ($# < 1) then
+	echo Usage: $0:t file_path
+	exit 1
+endif
+
+set file_path = "$1"
+set mls_folder = "$file_path:h"
+set org_file_folder = "$mls_folder:h"
+
+cd "$mls_folder"
+
+set rev_num = `echo "$file_path" | sed 's/.*_\([0-9]*\).*/\1/'`
+set org_file_name = `echo "$file_path" | sed 's/.*\(\/.*\)_[0-9]*\(.*\)/\1\2/'`
+set org_file_path = "${org_file_folder}${org_file_name}"
+
+# check if .cg exist
+if (! -e $meta_file_name) then
+	echo "no $meta_file_name meta file here"
+	${cg_folder}/alert "This is not a milestome of any file or folder"
+	exit 0
+endif
+
+# get the rev sha1 from the meta file
+set rev = `cat $meta_file_name | head -$rev_num | tail -1 | cut -d"-" -f1`
+
+git checkout $rev -- "$org_file_path"
+set commit_msg = `git log $rev -n 1 --format=%s -- "$org_file_path"`
+set commit_msg = "Revert to: $commit_msg"
+git commit -m "$commit_msg" -- "$org_file_path"
+${cg_folder}/change_file_comment "$org_file_path" "$commit_msg"
+rm -rf "$mls_folder"
+exit 0
--- view_milestones.sh
+++ view_milestones.sh
@@ -5,11 +5,15 @@ set cg_folder = ~/.cg
 
 # usage
 if ($# < 1) then
-	echo Usage: $0:t file_path
+	echo Usage: $0:t file_path [rev]
 	exit 1
 endif
 
 set file_path = "$1"
+
+if ($# == 2) then
+	set rev = "$2"
+endif
 
 # folder handling
 if (-d "$file_path") then
@@ -42,6 +46,27 @@ if ($status) then
 	${cg_folder}/alert "There are no milestones for this file"
 	exit 0
 endif
+
+# Revert to specific revison if a rev number provided as second arg
+# get back to n-th revision by spesifing n
+# get back n revision back by specifing -n
+if ($?rev) then
+	# go back to revision nunber
+	if ($rev > 0) then
+		set new_rev = `git log --reverse --format=%H -- $file_path | head -$rev | tail -1`
+	# go back $rev revison back
+	else if ($rev < 0) then
+		@ rev *= -1
+		set new_rev = "HEAD~$rev"
+	endif
+	git checkout $new_rev -- "$file_path"
+	set commit_msg = `git log $new_rev -n 1 --format=%s -- "$file_path"`
+	set commit_msg = "Revert to: $commit_msg"
+	git commit -m "$commit_msg" -- "$file_path"
+	${cg_folder}/change_file_comment "$file_path" "$commit_msg"
+	exit 0
+endif
+#	===== End Revert to specific revison ====
 
 # prepare a dir to put all milestones
 set mls_dir = "$file_path"_milestones
@@ -82,6 +107,10 @@ foreach r ($revs)
 	# modify the "Date modified" attribute of the file
 	set date = `git log $r -n 1 --format=%ci -- "$file_path" | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\) \([0-9]*\):\([0-9]*\):\([0-9]*\) .*/\1\2\3\4\5.\6/'`
 	touch -t $date "$new_file_name"
+
+	# add .cg file meta data
+	# <SHA1>-<Commiter Name>-<Date>-<Commit Messege>
+	git log $r -n 1 --format="%H-%cn-%ci-%s" -- "$file_path" >> "${mls_dir}/.cg"
 
 	@ i++
 end
