1032cc37b30a43fe20e7b684192ad9b8ee64aa67
Ido Fishler <idofishler@gmail.com>
2013-10-09 20:00:55 +0300

    Fixed font size in gui + attemp to focuse window

--- scripts/rengautils/gui.py
+++ scripts/rengautils/gui.py
@@ -15,6 +15,7 @@ class RengaGui(Tkinter.Tk):
         self.mainframe.columnconfigure(0, weight=2)
         self.mainframe.rowconfigure(0, weight=1)
         self.resizable(False, False)
+        self.font = ('Open Sans', 16)
 
     def done(self, event=None):
         self.result = self.cmt.get()
@@ -28,33 +29,36 @@ class RengaGui(Tkinter.Tk):
         self.lift()
         self.call('wm', 'attributes', '.', '-topmost', True)
         self.after_idle(self.call, 'wm', 'attributes', '.', '-topmost', False)
+        self.focus_force()
 
     def ask_for_comment(self):
-        ttk.Label(self.mainframe, text="What changes did you make?").grid(
+        ttk.Label(self.mainframe, font=self.font, text="What changes did you make?").grid(
             column=2, row=0, sticky="W")
         ttk.Button(self.mainframe, text="OK", command=self.done).grid(
             column=3, row=2, sticky="W")
         self.cmt = Tkinter.StringVar()
-        self.entry = ttk.Entry(self.mainframe, textvariable=self.cmt).grid(
-            column=0, row=1, columnspan=4, sticky="EW")
+        self.entry = ttk.Entry(self.mainframe, font=self.font, textvariable=self.cmt)
+        self.entry.grid(column=0, row=1, columnspan=4, sticky="EW")
+        self.entry.focus_set()
         self.bind("<Return>", self.done())
         self.bring_to_front()
 
     def alert(self, msg):
-        ttk.Label(self.mainframe, text=msg).grid(column=1, row=0, sticky="W")
-        ttk.Button(self.mainframe, text="OK", command=self.quit).grid(
-            column=3, row=1, sticky="W")
+        ttk.Label(self.mainframe, font=self.font, text=msg).grid(column=1, row=0, sticky="W")
+        button = ttk.Button(self.mainframe, text="OK", command=self.quit)
+        button.grid(column=3, row=1, sticky="W")
+        button.focus_set()
         self.bind("<Return>", self.quit())
         self.bring_to_front()
 
     def issue_report(self):
         info = 'Thanks for helping us improve RengaBit.\nPlease let us know what was the problem (short text is enough)\nIf you want us to contect you, sign with you name and email'
-        ttk.Label(self.mainframe, text=info).grid(column=1, row=0, sticky="W")
-        self.cmt = Tkinter.Text(self.mainframe, width=60, height=10)
+        ttk.Label(self.mainframe, font=self.font, text=info).grid(column=0, row=0, sticky="W")
+        self.cmt = Tkinter.Text(self.mainframe, font=self.font, width=60, height=10)
         self.cmt.grid(column=0, row=2, columnspan=3)
-        #self.entry = ttk.Entry(self.mainframe, textvariable=self.cmt).grid(column=0, row=2, columnspan=6, rowspan=4, sticky="EW")
-        ttk.Button(self.mainframe, text="Send", command=self.get_text).grid(
-            column=2, row=3, sticky="W")
+        self.cmt.focus_set()
+        button = ttk.Button(self.mainframe, text="Send", command=self.get_text)
+        button.grid(column=2, row=3, sticky="W")
         self.bring_to_front()
 
 if __name__ == "__main__":
