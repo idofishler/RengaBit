44b5884ac3d754ded3a5389369a6b324343834d3
Ido Fishler <idofishler@gmail.com>
2013-10-10 12:49:44 +0300

    Fix GUI issues
    * Text field is now highlighted (but application does not get top-most. looks like this is os dependent)
    * The window is now centred
    * Enter is pressing OK button.

--- scripts/rengautils/gui.py
+++ scripts/rengautils/gui.py
@@ -17,6 +17,14 @@ class RengaGui(Tkinter.Tk):
         self.resizable(False, False)
         self.font = ('Open Sans', 16)
 
+    def center(self):
+        self.update_idletasks()
+        width = self.winfo_width()
+        height = self.winfo_height()
+        x = (self.winfo_screenwidth() / 2) - (width / 2)
+        y = (self.winfo_screenheight() / 2) - (height / 2)
+        self.geometry('{0}x{1}+{2}+{3}'.format(width, height, x, y))
+
     def done(self, event=None):
         self.result = self.cmt.get()
         self.quit()
@@ -26,10 +34,10 @@ class RengaGui(Tkinter.Tk):
         self.quit()
 
     def bring_to_front(self):
+        self.center()
         self.lift()
         self.call('wm', 'attributes', '.', '-topmost', True)
         self.after_idle(self.call, 'wm', 'attributes', '.', '-topmost', False)
-        self.focus_force()
 
     def ask_for_comment(self):
         ttk.Label(self.mainframe, font=self.font, text="What changes did you make?").grid(
@@ -37,24 +45,28 @@ class RengaGui(Tkinter.Tk):
         ttk.Button(self.mainframe, text="OK", command=self.done).grid(
             column=3, row=2, sticky="W")
         self.cmt = Tkinter.StringVar()
-        self.entry = ttk.Entry(self.mainframe, font=self.font, textvariable=self.cmt)
+        self.entry = ttk.Entry(
+            self.mainframe, font=self.font, textvariable=self.cmt)
         self.entry.grid(column=0, row=1, columnspan=4, sticky="EW")
         self.entry.focus_set()
-        self.bind("<Return>", self.done())
+        self.entry.bind("<Return>", lambda e: self.done())
         self.bring_to_front()
 
     def alert(self, msg):
-        ttk.Label(self.mainframe, font=self.font, text=msg).grid(column=1, row=0, sticky="W")
+        ttk.Label(self.mainframe, font=self.font, text=msg).grid(
+            column=1, row=0, sticky="W")
         button = ttk.Button(self.mainframe, text="OK", command=self.quit)
         button.grid(column=3, row=1, sticky="W")
         button.focus_set()
-        self.bind("<Return>", self.quit())
+        button.bind("<Return>", lambda e: self.quit())
         self.bring_to_front()
 
     def issue_report(self):
         info = 'Thanks for helping us improve RengaBit.\nPlease let us know what was the problem (short text is enough)\nIf you want us to contect you, sign with you name and email'
-        ttk.Label(self.mainframe, font=self.font, text=info).grid(column=0, row=0, sticky="W")
-        self.cmt = Tkinter.Text(self.mainframe, font=self.font, width=60, height=10)
+        ttk.Label(self.mainframe, font=self.font, text=info).grid(
+            column=0, row=0, sticky="W")
+        self.cmt = Tkinter.Text(
+            self.mainframe, font=self.font, width=60, height=10)
         self.cmt.grid(column=0, row=2, columnspan=3)
         self.cmt.focus_set()
         button = ttk.Button(self.mainframe, text="Send", command=self.get_text)
